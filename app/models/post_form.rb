6b689f02824198a5fc8090a2acdd7d83272f86bb
hossan09 <hossan9.saito@gmail.com>
2023-01-03 16:47:27 +0900

    投稿した内容にタグを付与

---
+++ app/models/post_form.rb
@@ -0,0 +1,26 @@
+class PostForm
+  include ActiveModel::Model
+
+  #PostFormクラスのオブジェクトがPostモデルの属性を扱えるようにする
+  attr_accessor(
+    :text, :image,
+    :id, :created_at, :updated_at,
+    :tag_name
+   )
+
+  with_options presence: true do
+    validates :text
+    validates :image
+  end
+
+  def save
+    post = Post.create(text: text, image: image)
+    tag = Tag.where(tag_name: tag_name).first_or_initialize
+    tag.save
+    PostTagRelation.create(post_id: post.id, tag_id: tag.id)
+  end
+
+  def update(params, post)
+    post.update(params)
+  end
+end
